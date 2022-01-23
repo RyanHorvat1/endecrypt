e3b1b8d21e105b060976543f7607ab392ed72336
Ryan Horvat <ryanhorvat.oracle@outlook.com>
2022-01-23 10:20:20 +0000

    Minor fixes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ const output = document.getElementById("output");
 
 /*CAPTURA DE TEXTO EN EL INPUT*/
 input.addEventListener("input", function() {
-    input.value = input.value.replace(/[^A-Za-z \r\n¡!¿?]/g, '');
+    input.value = input.value.replace(/[^A-Za-z \r\n¡!¿?.,;:]/g, '');
     input.value = input.value.toLowerCase();
 });
